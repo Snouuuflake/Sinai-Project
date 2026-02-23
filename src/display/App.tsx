4b19c2e737acf87e7b542e36a8bb97f1928b9a0a
Snouuuflake <xuehuamusic2@gmail.com>
2026-02-22 23:26:43 -0600

    added f key as fullscreen for display windows

--- src/display/App.tsx
+++ src/display/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import Body from "./Body";
 import { DisplayConfigStateContextProvider } from "./DisplayConfigStateContext";
 
 const App: React.FC<{}> = ({ }) => {
+  useEffect(() => {
+    const listener = (event: KeyboardEvent) => {
+      if (event.key === "f") {
+        window.document.documentElement.requestFullscreen();
+      }
+    };
+    window.addEventListener("keydown", listener)
+    return () => {
+      window.removeEventListener("keydown", listener);
+    }
+  }, [])
   return <DisplayConfigStateContextProvider>
     <Body />
   </DisplayConfigStateContextProvider>
